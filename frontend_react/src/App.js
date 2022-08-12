edf498e048798f990253c05ce175a88f7f82ba5b
TuckerBrady <tuck.al.brady@gmail.com>
2022-08-11 21:38:43 -0600

    sets up the start of the navbar.

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+import { About, Blog, Footer, Header, Podcast, Shop } from './container';
+import { Navbar } from './components';
+import './App.scss';
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <Navbar />
+      <Header />
+      <About />
+      <Blog />
+      <Podcast />
+      <Shop />
+      <Footer />
+    </div>
   );
 }
 
