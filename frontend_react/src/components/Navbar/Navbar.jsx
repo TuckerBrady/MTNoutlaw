4d10dd2fb2df60e8adc5a7f23c943747ec1c7801
TuckerBrady <tuck.al.brady@gmail.com>
2022-08-14 23:25:46 -0600

    creates the hamburger menu at 1k px.

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { GiHamburger, GiArrowCluster } from 'react-icons/gi';
+// import { motion } from "framer-motion";
 
 import { images } from '../../constants';
 import './Navbar.scss';
 const Navbar = () => {
+    const [toggle, setToggle] = useState(false);
+
   return (
     <nav className='app__navbar'>
         <div className='app__navbar-logo'>
@@ -15,6 +19,23 @@ const Navbar = () => {
                 </li>
             ))}
         </ul>
+
+        <div className='app__navbar-menu'>
+            <GiHamburger onClick={() => setToggle(true)} />
+
+            {toggle && (
+                <div>
+                    <GiArrowCluster onClick={() => setToggle(false)} />
+                    <ul className='app__navbar-links'>
+                        {['Home', 'About', 'Learn', 'Podcast', 'Shop', 'Contact'].map((item) => (
+                            <li key={item}>
+                                <a href={`${item}`} onClick={() => setToggle(false)}>{item}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </div>
     </nav>
   );
 }
