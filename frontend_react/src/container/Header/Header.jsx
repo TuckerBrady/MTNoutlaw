083221273c05c5b429621c54d942211799292898
TuckerBrady <tuck.al.brady@gmail.com>
2022-08-27 08:40:08 -0600

    hamburger menu.

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
 
 import './Header.scss';
+import { images } from '../../constants';
+
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0,1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut'
+    }
+  }
+}
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="app__header app__flex">
+
+    </div>
   )
 }
 
