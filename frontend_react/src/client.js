29d3fc205754e40615fdcaf683eee6e609783aa3
TuckerBrady <tuck.al.brady@gmail.com>
2022-08-28 23:00:09 -0600

    updates and connects the env file.

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-    projectId: 'rj79se6o',
+    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
     apiVersion: '2022-02-01',
     useCdn: true,
-    token: 'skUU6RLNgLEgyZYJLlmJFXb9n5NUyLGBWD60Bj30fl559YxR7RpzOB7eFrI43x32VhIuaTIGXBOuVzOtJSRJTP8OHqLaQJXTuvGGtbjsmg7BxRdKqopBOfpNT5o2e03ZwqTH82E9HLavnnlrpLweSmcz4Zmn5Pdur7ULgMaNFTYceRnNdkix',
+    token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
