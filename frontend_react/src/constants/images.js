37aa1bde6eb02e04a7209b2c022358cd0dbc7fec
TuckerBrady <tuck.al.brady@gmail.com>
2022-09-05 21:15:48 -0600

    finishes up the podcast section of the homepage

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -2,10 +2,16 @@ import logo from '../assets/logo.gif';
 import microphone from '../assets/microphone.gif';
 import campfire from '../assets/campfire.png';
 import social from '../assets/social.png';
+import applePodcasts from '../assets/applePodcast.gif';
+import spotifyPodcasts from '../assets/spotifyPodcast.gif'
+import anchorPodcasts from '../assets/anchorPodcast.gif';
 
 export default {
     logo,
     microphone,
     campfire,
-    social
+    social,
+    applePodcasts,
+    spotifyPodcasts,
+    anchorPodcasts
 }
--- frontend_react/src/container/Podcast/Podcast.jsx
+++ frontend_react/src/container/Podcast/Podcast.jsx
@@ -1,10 +1,85 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
 
 import { AppWrap, MotionWrap } from '../../wrapper';
+import { images } from '../../constants';
 import './Podcast.scss';
+
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0,1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut'
+    }
+  }
+}
+
 const Podcast = () => {
+  // const [podList, setPodList] = useState([]);
+
+  // useEffect(() => {
+  //   const query = '*[_type == "about"]'
+  //   client.fetch(query)
+  //     .then((data) => setaboutList(data))
+  // }, []);
+
+  
+
   return (
-    <div>Podcast</div>
+    <>
+      <h2 className="head-text">
+        The Latest <span>MTN Outlaws Podcast</span> Episode
+      </h2>
+      <div className="app__podcasts">
+        <motion.div
+          whileInView={{ opacity: 1 }}
+          className="app__podcast-item"
+        >
+          <iframe className="responsive-iframe" src="https://anchor.fm/tucker-al-brady/embed/episodes/Episode-5----Colorado-Sucks--2021-Hunting-Application-Strategy--and-LARPing-eps49v" height="200px" width="850px" frameborder="0" scrolling="no"></iframe>
+        </motion.div>
+      </div>
+
+      <motion.div
+        variant={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__podcast-listen-squares"
+      >
+          <motion.div 
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.05, type: 'tween' }}
+            className="podcast-listen-on app__flex"
+          >
+            <a href="https://podcasts.apple.com/us/podcast/mtn-outlaw/id1540813137" target="blank">
+              <img src={images.applePodcasts} alt="apple-podcasts" />
+            </a>
+          </motion.div>
+          <motion.div 
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.05, type: 'tween' }}
+            className="podcast-listen-on app__flex"
+          >
+            <a href="https://open.spotify.com/show/70sqJb5Wt6BfUc8q69lWmJ" target="blank">
+              <img src={images.spotifyPodcasts} alt="spotify-podcasts" />
+            </a>
+          </motion.div>
+          <motion.div 
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.05, type: 'tween' }}
+            className="podcast-listen-on app__flex"
+          >
+            <a href="https://anchor.fm/mtnoutlaw" target="blank">
+              <img src={images.anchorPodcasts} alt="anchor-podcasts" />
+            </a>
+          </motion.div>
+      </motion.div>
+
+      <div className="head-text">
+        Subscribe and Leave a <span>5 Star</span> Review
+      </div>
+
+    </>
   )
 }
 
